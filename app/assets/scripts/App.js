734ff543be693500798cc94d793f5409d81c4077
Javier <javcarroz89@gmail.com>
2018-01-16 01:59:47 -0400

    Working on new js features.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,3 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person'); old way
-import Person from './modules/Person'; // new EC6 way!
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-  payTaxes(){
-    console.log(this.name + " has become an Adult, go pay your taxes!");
-  }
-}
-
-alert('Testing 1,2,3,4!');
-
-var john = new Person ("John Doe", "blue");
-
-
-var jane = new Adult("Jane Smith", "yellow");
-jane.payTaxes();
-console.log(jane.favoriteColor.toString());
+var mobileMenu = new MobileMenu();
